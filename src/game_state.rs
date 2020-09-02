e91ce5642fe5da837b28b46915c74fee7a99c4d7
André Claudino <claudino@d2x.com.br>
2020-09-01 22:06:50 -0300

    Exibe pontuação na tela

--- src/game_state.rs
+++ src/game_state.rs
@@ -1,4 +1,5 @@
-use tetra::graphics::{self, Color, Texture};
+use tetra::graphics::{self, Color, Texture, DrawParams};
+use tetra::graphics::text::{Font, Text};
 use tetra::math::Vec2;
 use tetra::{Context, State};
 use tetra::input::{self, Key};
@@ -10,6 +11,12 @@ pub struct GameState {
     player1: Entity,
     player2: Entity,
     ball: Entity,
+
+    score1: Text,
+    score2: Text,
+
+    score1_position: Vec2<f32>,
+    score2_position: Vec2<f32>
 }
 
 impl GameState {
@@ -34,10 +41,24 @@ impl GameState {
             (WINDOW_HEIGHT - ball_texture.height() as f32)/2.0
         );
 
+        let score1 = graphics::text::Text::new(format!("{}", 0),
+            Font::vector(ctx, "./resources/joystix.ttf", 100.0)?
+        );
+        let score1_position = Vec2::new(16.0, 0.0);
+        
+        let score2 = graphics::text::Text::new(format!("{}", 0),
+            Font::vector(ctx, "./resources/joystix.ttf", 100.0)?
+        );
+        let score2_position = Vec2::new(WINDOW_WIDTH - 84.0, 0.0);
+
         Ok(GameState {
             player1: Entity::new(player1_texture, player1_position),
             player2: Entity::new(player2_texture, player2_position),
-            ball: Entity::with_velocity(ball_texture, ball_position, ball_velocity)
+            ball: Entity::with_velocity(ball_texture, ball_position, ball_velocity),
+            
+            score1, score2,
+            score1_position,
+            score2_position
         })
     }
 
@@ -71,10 +92,17 @@ impl State for GameState {
         graphics::draw(ctx, &self.player1.texture, self.player1.position);
         graphics::draw(ctx, &self.player2.texture, self.player2.position);
         graphics::draw(ctx, &self.ball.texture, self.ball.position);
-        
-        let text = graphics::text::Text::new(,
-            Font::vector(ctx, "./examples/resources/DejaVuSansMono.ttf", 16.0)?);
 
+        let draw_params1 = DrawParams::new()
+            .color(Color::rgb(1.0, 1.0, 1.0))
+            .position(self.score1_position);
+        graphics::draw(ctx, &self.score1, draw_params1);
+
+        let draw_params2 = DrawParams::new()
+            .color(Color::rgb(1.0, 1.0, 1.0))
+            .position(self.score2_position);
+        graphics::draw(ctx, &self.score2, draw_params2);
+        
         self.ball.position += self.ball.velocity;
 
         Ok(())
@@ -97,7 +125,7 @@ impl State for GameState {
         if input::is_key_down(ctx, Key::Down) {
             self.player2.position.y += PADDLE_SPEED;
         }
-
+    
         let player1_bounds = self.player1.bounds();
         let player2_bounds = self.player2.bounds();
         let ball_bounds = self.ball.bounds();
@@ -125,12 +153,17 @@ impl State for GameState {
         if self.ball.position.x < 0.0 {
             self.reset(-1);
             self.player2.score += 1;
+            self.score2.set_content(format!("{}", self.player2.score));
+            if let Some(optional_score2) = self.score2.get_bounds(ctx) {
+                self.score2_position.x = WINDOW_WIDTH - (16.0 + optional_score2.width);
+            }
             println!("Player 2 wins!");
         }
 
         if self.ball.position.x > WINDOW_WIDTH {
             self.reset(1);
             self.player1.score += 1;
+            self.score1.set_content(format!("{}", self.player1.score));
             println!("Player 1 wins!");
         }
 
